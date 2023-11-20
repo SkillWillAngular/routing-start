75f399bcb349c1a910687f28d8cd36e70aeef7ad
anadiradze <anadi16@freeuni.edu.ge>
2023-11-21 01:20:17 +0400

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductsModule } from './products/products.module';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CartComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ProductsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
